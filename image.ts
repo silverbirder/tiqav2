11d0f66c3b3ae34165a63991e9ad4e2a4b3ac185
shibamoto <silverbirder@gmail.com>
2019-11-25 21:57:20 +0900

    add typescripts file

--- image.ts
+++ image.ts
@@ -1,5 +1,21 @@
 abstract class Image {
-    abstract save(): void;
+    abstract async save(): Promise<void>;
 }
 
-export default Image;
+class CloudinaryImage extends Image {
+    private cloudinary: any;
+    constructor() {
+        super();
+        this.cloudinary = require('cloudinary').v2;
+        this.cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
+        })
+    }
+    async save(): Promise<void>{
+        await this.cloudinary.uploader.upload("./my_image.jpg");
+        return;
+    }
+}
+export = {CloudinaryImage, Image};
---
+++ index.ts
@@ -0,0 +1,4 @@
+const {CloudinaryImage, Image} = require('./image')
+
+const image = new CloudinaryImage();
+image.save();
