12cf531538a3c38f5be4a9ca9f3c7f4f29b39b76
silverbirder <silverbirder@gmail.com>
2019-12-01 10:29:18 +0000

    add routes

--- index.ts
+++ index.ts
@@ -3,12 +3,22 @@ import {CloudinaryImage} from './image/cloudinaryImage'
 import express from 'express';
 
 const app = express();
-app.get('/', (req, res) => {
-    if (req.query.url) {
-        let image: IImage;
-        image = new CloudinaryImage();
-        image.save(req.query.url);
+app.get('/save', (req, res) => {
+    if (!req.query.url) {
+        res.send('not exit query parameter: url');
     }
+    let image: IImage;
+    image = new CloudinaryImage();
+    image.save(req.query.url);
     res.send('end');
+});
+app.get('/search', async (req, res) => {
+    if (!req.query.q) {
+        res.send('not exit query parameter: q');
+    }
+    let image: IImage;
+    image = new CloudinaryImage();
+    const result: any = await image.search(req.query.q);
+    res.json(result.hits);
 });
 app.listen(8080, () => console.log('Listening on port 8080'));
