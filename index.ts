32749126e577e024d82bbd960bbb8d03ed435590
silverbirder <silverbirder@gmail.com>
2019-12-01 08:34:30 +0000

    add express

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import { IImage } from './image/i_image'
 import {CloudinaryImage} from './image/cloudinaryImage'
+import express from 'express';
 
-let image: IImage;
-image = new CloudinaryImage();
-image.save('');
-// console.logimage.get('mrfvbud0m4z0jeklxcaa'));
-// image.text('./my_image.jpg');
-// image.search('LGTM').then(value => console.log(value));
+const app = express();
+app.get('/', (req, res) => {
+    if (req.query.url) {
+        let image: IImage;
+        image = new CloudinaryImage();
+        image.save(req.query.url);
+    }
+    res.send('end');
+});
+app.listen(8080, () => console.log('Listening on port 8080'));
