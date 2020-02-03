b9dc01d0a50b4bddf4885c82e9164f81b2e56688
shibamoto <silverbirder@gmail.com>
2020-02-04 07:38:24 +0900

    Refactoring test code

--- __tests__/src/3_interface_adapters/gateways/searchGatewayImpl.test.ts
+++ __tests__/src/3_interface_adapters/gateways/searchGatewayImpl.test.ts
@@ -1,10 +1,16 @@
 import {SearchGatewayImpl} from "../../../../src/3_interface_adapters/gateways/searchGatewayImpl";
 import {ISearchGateway} from "../../../../src/2_application_business_rules/gateways/iSearchGateway";
 import {ImageEntityImpl} from "../../../../src/1_enterprise_business_rules/entities/imageEntityImpl";
-import Spy = jasmine.Spy;
+
 
 describe('Class: SearchGatewayImpl', () => {
-    // Arrange
+    beforeEach(() => {
+        setEnv();
+    });
+    afterEach(() => {
+        unSetEnv();
+    });
+
     function createSearchGateway(): ISearchGateway {
         return new SearchGatewayImpl();
     };
@@ -14,9 +20,19 @@ describe('Class: SearchGatewayImpl', () => {
         process.env.ALGOLIA_ADMIN_KEY = 'ALGOLIA_ADMIN_KEY';
         process.env.ALGOLIA_INDEX_NAME = 'ALGOLIA_INDEX_NAME';
     };
+
+    function unSetEnv(): void {
+        delete process.env.ALGOLIA_APP_ID;
+        delete process.env.ALGOLIA_ADMIN_KEY;
+        delete process.env.ALGOLIA_INDEX_NAME;
+    };
+
     describe('Method: constructor', () => {
         describe('Env: no env', () => {
             it('Assert: Error', () => {
+                // Arrange
+                unSetEnv();
+
                 // Act & Assert
                 expect(createSearchGateway).toThrow();
             });
@@ -35,11 +51,12 @@ describe('Class: SearchGatewayImpl', () => {
         describe('Args: No', () => {
             it('Assert: No results', async () => {
                 // Arrange
-                setEnv();
                 const searchGateway: ISearchGateway = createSearchGateway();
                 // @ts-ignore
                 const searchMock: jest.Spy = jest.spyOn(searchGateway.alogoliaAdminIndex, 'search');
-                searchMock.mockImplementationOnce(() => {return {hits: []}});
+                searchMock.mockImplementationOnce(() => {
+                    return {hits: []}
+                });
 
                 // Act
                 const results: Array<ImageEntityImpl> = await searchGateway.search(0, '', []);
